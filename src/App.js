9cebb62c129ed3221104e9efdcd9b909ea7cd442
Angel Mendez <hello@finangraphy.com>
2019-12-17 17:05:25 -0600

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Homepage from './components/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component'
 import './App.css';
 
 function App() {
