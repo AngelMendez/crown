c95f6be903294dee8984f4e8da6b7ce055ec280b
Angel Mendez <hello@finangraphy.com>
2019-12-22 14:28:10 -0600

    Implemented Redirect

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import store from './redux/store';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />,
-        </BrowserRouter>,
+            <App />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'),
 );
