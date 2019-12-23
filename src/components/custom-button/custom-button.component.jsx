63175ed16609003646ad9e84a829566e99d5f51d
Angel Mendez <hello@finangraphy.com>
2019-12-23 09:43:33 -0600

    updated shop data with new data added cart utils

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`}
-        {...otherProps}
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={
+        `
+            ${ isGoogleSignIn ? 'google-sign-in': '' } 
+            ${ inverted ? 'inverted': '' } 
+            custom-button
+        `
+    }
+        { ...otherProps }
     >
-        {children}
+        { children }
     </button>
 )
 
